8256a44b445bdf2980e87f5de1b18b2ec99fe3e5
ryuzera <ryugfx2@gmail.com>
2025-04-06 09:36:10 -0300

    Criação da tabela de Activities e da entidade que irá representar uma Activity

--- src/main/resources/db/migration/V2__create-table-participants.sql
+++ src/main/resources/db/migration/V2__create-table-participants.sql
@@ -3,5 +3,5 @@ CREATE TABLE participants (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     is_confirmed BOOLEAN NOT NULL,
-    trip_id UUID, FOREIGN KEY (trip_id) REFERENCES trips(id)
+    trip_id UUID, FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE
 );
